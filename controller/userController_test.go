3108d144b01777c6ec55c146fa1badad460239ff
Igor Ferreira de Almeida <igor.rigel@gmail.com>
2020-08-23 18:42:03 -0300

    Add new unit tests

--- controller/userController_test.go
+++ controller/userController_test.go
@@ -11,53 +11,6 @@ import (
 	"testing"
 )
 
-//func TestCreateUser(t *testing.T) {
-//	tests := []struct {
-//		tag            string
-//		expectedStatus int
-//	}{
-//		{
-//			tag:            "t1 create valid user",
-//			expectedStatus: http.StatusCreated,
-//		},
-//	}
-//
-//	for _, test := range tests {
-//		var jsonStr = []byte(`{"name":"Igor", "age":31, "gender":"male"}`)
-//		router := gin.Default()
-//		req, err := http.NewRequest("POST", "/users", bytes.NewBuffer(jsonStr))
-//
-//		if err != nil {
-//			t.Fatal(err)
-//		}
-//
-//		req.Header.Set("Content-Type", "application/json")
-//		rr := httptest.NewRecorder()
-//
-//		// Mock
-//		//user := usermd.User{"name1", 1, "male"}
-//		//mockService := new(MockService)
-//		//mockService.On("Add").Return(user)
-//		// userService := mockService
-//		// userService.Add(user)
-//
-//		router.POST("/users", CreateUser)
-//		router.ServeHTTP(rr, req)
-//
-//		var userDTO dto.UserDTO
-//		err = json.Unmarshal(rr.Body.Bytes(), &userDTO)
-//
-//		fmt.Println(userDTO)
-//
-//		assert.Nil(t, err)
-//		assert.NotNil(t, userDTO)
-//		assert.EqualValues(t, test.expectedStatus, rr.Code)
-//		assert.EqualValues(t, "Igor", userDTO.Name)
-//		assert.EqualValues(t, 31, userDTO.Age)
-//		assert.EqualValues(t, "male", userDTO.Gender)
-//	}
-//}
-
 func TestFindUser(t *testing.T) {
 	response := httptest.NewRecorder()
 	context, _ := gin.CreateTestContext(response)
@@ -111,4 +64,28 @@ func TestUpdateUser(t *testing.T) {
 	assert.EqualValues(t, "Melissa", responseDTO.Name)
 	assert.EqualValues(t, 16, responseDTO.Age)
 	assert.EqualValues(t, "female", responseDTO.Gender)
+}
+
+func TestUpdateUserBadIdParam(t *testing.T) {
+	response := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(response)
+
+	context.Params = gin.Params{
+		{Key: "id", Value: "adfadsf"},
+	}
+	var jsonStr = []byte(`{"name":"Melissa", "age":16, "gender":"female"}`)
+	request, _ := http.NewRequest("PUT", "/users/:id", bytes.NewBuffer(jsonStr))
+	context.Request = request
+
+	UpdateUser(context)
+
+	var responseDTO dto.ErrorResponseDTO
+	err := json.Unmarshal(response.Body.Bytes(), &responseDTO)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, responseDTO)
+	assert.EqualValues(t, response.Code, http.StatusBadRequest)
+	assert.EqualValues(t, responseDTO.HttpStatus, http.StatusText(http.StatusBadRequest))
+	assert.EqualValues(t, responseDTO.Code, http.StatusBadRequest)
+	assert.EqualValues(t, responseDTO.Message, "Invalid param for id")
 }
